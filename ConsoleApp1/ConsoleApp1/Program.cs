56d9bc1778851dab8b06ec14b1966c452695a5bf
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-09-19 18:55:46 +0300

    Создал массив с 3мя данными, почле чего вывел их. Создал второй массив и присвоил ему данные первого массива в обратном порядке

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] A = new string[3] { "размарин", "мясо", "соль" };
+            A[2] = "собака";
+            Console.WriteLine(A[0]);
+            Console.WriteLine(A[1]);
+            Console.WriteLine(A[2]);
+
+            string[] B = new string[3];
+            B[0] = A[2];
+            B[1] = A[1];
+            B[2] = A[0];
+            Console.WriteLine(B[0]);
+            Console.WriteLine(B[1]);
+            Console.WriteLine(B[2]);
+        }
+    }
+}
