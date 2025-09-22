b042ad5fbb71e24dde92a3f6b0a9a74dbb0aaa79
ArseniyRyzhov <ryzovarseniy7@gmail.com>
2025-09-22 23:31:21 +0300

    добавил элементы в пустой список и измерило размер списка после добавления элементов

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,7 +10,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            List <double> list = new List<double>();
+            List <double> list = new List<double>(); ///создал пустой список без элементов
+            Console.WriteLine("Исходный размер списка: " + list.Count);
+
+            ///добавляю элементы в список 
+            list.Add(1.0);
+            list.Add(1.22);
+            list.Add(22.33);
+            list.Add(0.0);
+            list.Add(-1.0);
+            Console.WriteLine("Размер списка после добавления элементов: " + list.Count);
+
+
         }
     }
 }
