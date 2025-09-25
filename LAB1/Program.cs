d5bca9578a83767820408c513da6bfdf99cf9df5
mariya-koz <mkozyreva868@gmail.com>
2025-09-25 09:03:09 +0300

    дал объяснение

--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -13,21 +13,22 @@ namespace LAB1
             // Вариант 1 Рыжов Арсений 
             Console.WriteLine("Введите размер массива: ");
             int Length = Convert.ToInt32(Console.ReadLine());
-            int[] Array = new int[Length];
+            int[] Array = new int[Length]; /// даю возможность ввести размер массива
             Console.WriteLine("Введите элемент массива:");
-            for (int i = 0; i < Length; i++)
+            for (int i = 0; i < Length; i++) /// даю возможность ввести значения для массива
             {
                 Array[i] = Convert.ToInt32(Console.ReadLine());
             }
             int sum = 0;
             for (int i = 0; i < Length; i++)
             {
-                if (Array[i] > 0)
+                if (Array[i] > 0) /// Отбираем положительные элементы массива 
                 {
-                    sum += Array[i];
+                    sum += Array[i]; /// суммирую положительные элементы массива 
                 }
             }
             Console.WriteLine("Сумма положительных элементов:" + sum);
+            Console.ReadKey();
          
         }
     }
