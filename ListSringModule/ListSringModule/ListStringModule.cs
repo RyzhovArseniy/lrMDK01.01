1db4281430920fe0dc19b8fce4e7d05401e2780c
Kate <20katerina0060@gmail.com>
2025-09-27 13:02:34 +0300

    доделать

---
+++ ListSringModule/ListSringModule/ListStringModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListSringModule
+{
+    public class ListStringModule
+    {
+         static public List<string> resultList()
+         {
+            Console.Write("Введите список дел:");
+            int imput = Convert.ToInt32(Console.ReadLine());
+            List<string> list = new List<String>();
+            for (int i = 0; i< imput; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            return list;
+
+         }
+        static public List<string> numbers(List<string> list, string text)
+        {
+           
+            
+            List<int> result = new List<int>();
+            
+            return result;
+        }
+
+
+
+
+    }
+}
---
+++ ListSringModule/ListSringModule/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace ListSringModule
+{
+    class Program
+    {
+        static public void Main()
+        {
+            List<string> value = ListStringModule.resultList();
+            Console.WriteLine(value);
+            Console.WriteLine("Список:" + (value.Count - 1));
+
+            List<string> text = ListStringModule.resultList();
+        }
+    }
+}
